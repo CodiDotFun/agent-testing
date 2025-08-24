c54814725b8ca1eb453be5c331d1c6d5fa4d5265
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-23 22:28:56 -0600

    Update main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -5,3 +5,12 @@ use command::Command;
 mod command;
 pub mod utils;
 
+#[derive(Parser)]
+struct App {
+    #[command(subcommand)]
+    command: Command,
+}
+
+#[tokio::main]
+async fn main() -> eyre::Result<()> {
+    let app = App::parse();
