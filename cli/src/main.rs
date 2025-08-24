5598d80c1a095c897c33199742132d7a73401904
Venom 🎱 <155748950+88qji@users.noreply.github.com>
2025-08-23 22:29:02 -0600

    Update main.rs

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -14,3 +14,16 @@ struct App {
 #[tokio::main]
 async fn main() -> eyre::Result<()> {
     let app = App::parse();
+ match app.command {
+        Command::ProgramConfigInit(command) => command.execute().await,
+        Command::MultisigCreate(command) => command.execute().await,
+        Command::ProposalVote(command) => command.execute().await,
+        Command::VaultTransactionExecute(command) => command.execute().await,
+        Command::VaultTransactionCreate(command) => command.execute().await,
+        Command::ConfigTransactionCreate(command) => command.execute().await,
+        Command::ConfigTransactionExecute(command) => command.execute().await,
+        Command::VaultTransactionAccountsClose(command) => command.execute().await,
+        Command::InitiateTransfer(command) => command.execute().await,
+        Command::InitiateProgramUpgrade(command) => command.execute().await,
+    }
+}
